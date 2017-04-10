2ffa5e12a9a81aa9e517f3220fffe16419bc6a97
PhiLe <phi2902@yahoo.com>
2017-04-11 00:11:58 +0300

    Validation file uploaded , formatxmltojson and add a json pattern before importing to database

--- app/Http/Controllers/TVController.php
+++ app/Http/Controllers/TVController.php
@@ -6,6 +6,8 @@ use Illuminate\Http\Request;
 use App\Repositories\ChannelRepository;
 use App\Repositories\ProgramRepository;
 use App\Repositories\ScheduleRepository;
+use App\StaticClass\Xml;
+use Illuminate\Support\Facades\Validator;
 
 class TVController extends Controller
 {
@@ -22,78 +24,20 @@ class TVController extends Controller
         $this->scheduleRepository = $scheduleRepository;
     }
     
-    public function formatAttribute($data,array $array)
-    {
-        foreach ($data as $key => $value)
-        {
-            $array[$key] = (string)$value;    
-        }
-        return $array;
-    }
-    
-    public function formatXmlToJson($string)
-    {
-        $xml = simplexml_load_string($string);
-        $array = array();
-        $array['network'] = array();
-        //var_dump($xml);
-        foreach ($xml->network as $network) {
-            $temp_network = array();
-            $temp_network['service'] = array();
-            $temp_network = $this->formatAttribute($network->attributes(), $temp_network);
-            foreach ($network->service as $service) {
-                $temp_service = array();
-                $temp_service['event'] = array();
-                $temp_service = $this->formatAttribute($service->attributes(), $temp_service);
-                foreach ($service->event as $event) {
-                    $temp_event = array();
-                    $temp_event['language'] = array();
-                    $temp_event['content'] = array();
-                    $temp_event['parental_rating'] = array();
-                    $temp_event = $this->formatAttribute($event->attributes(), $temp_event);
-                    foreach ($event->language as $language) {
-                        $temp_language = array();
-                        $temp_language['short_event'] = array();
-                        $temp_language = $this->formatAttribute($language->attributes(), $temp_language);
-                        foreach ($language->short_event as $short_event) {
-                            $temp_short_event = array();
-                            $temp_short_event = $this->formatAttribute($short_event->attributes(), $temp_short_event);
-                            array_push($temp_language['short_event'], $temp_short_event);
-                        }
-                        array_push($temp_event['language'], $temp_language);
-                    }
-
-                    foreach ($event->content as $content) {
-                        $temp_content = array();
-                        $temp_content = $this->formatAttribute($content->attributes(), $temp_content);
-                        array_push($temp_event['content'], $temp_content);
-                    }
-
-                    foreach ($event->parental_rating as $parental_rating) {
-                        $temp_parental_rating = array();
-                        $temp_parental_rating['country'] = array();
-                        $temp_parental_rating = $this->formatAttribute($parental_rating->attributes(), $temp_parental_rating);
-                        foreach ($language->country as $country) {
-                            $temp_country = array();
-                            $temp_country = $this->formatAttribute($country->attributes(), $temp_country);
-                            array_push($temp_language['country'], $temp_country);
-                        }
-                        array_push($temp_event['parental_rating'], $temp_parental_rating);
-                    }
-                    array_push($temp_service['event'], $temp_event);
-                }
-                array_push($temp_network['service'], $temp_service);
-            }
-            array_push($array['network'], $temp_network);
-        }
-        return json_encode($array);
-    }
-
     public function executeFile(Request $request)
     {
+        $validator = Validator::make($request->all(), [
+                'inputFile' => 'mimes:json,xml,csv',
+        ]);
+
+        if ($validator->fails()) {
+            return redirect()->back()->withErrors($validator)->withInput();
+        }
+        
+        $extension = $this->getExtension($request->inputFile->getClientMimeType());
         $string = file_get_contents($request->inputFile);
-        $json = $this->formatXmlToJson($string);
-      
+        $json = $this->formatToJson($extension, $string);
+        
         try {
             $this->channelRepository->create($json);
             $this->programRepository->create($json);
@@ -103,6 +47,26 @@ class TVController extends Controller
             echo $ex->getMessage();
         }
 
-        //echo "Good Job";
+    }
+    
+    public function getExtension($param)
+    {
+        $array = explode('/',$param);
+        return $array[1];
+    }
+    
+    public function formatToJson($extension,$string)
+    {
+        switch ($extension) {
+            case 'xml':
+                return Xml::formatXmlToJson($string);
+                break;
+            case 'json':
+                break;
+            case 'csv':
+                break;
+            default:
+                break;
+        }
     }
 }
---
+++ app/StaticClass/Xml.php
@@ -0,0 +1,72 @@
+<?php
+namespace App\StaticClass;
+
+class Xml
+{
+    static function formatAttribute($data,array $array)
+    {
+        foreach ($data as $key => $value)
+        {
+            $array[$key] = (string)$value;    
+        }
+        return $array;
+    }
+    
+    static function formatXmlToJson($string)
+    {
+        $xml = simplexml_load_string($string);
+        $array = array();
+        $array['network'] = array();
+        //var_dump($xml);
+        foreach ($xml->network as $network) {
+            $temp_network = array();
+            $temp_network['service'] = array();
+            $temp_network = self::formatAttribute($network->attributes(), $temp_network);
+            foreach ($network->service as $service) {
+                $temp_service = array();
+                $temp_service['event'] = array();
+                $temp_service = self::formatAttribute($service->attributes(), $temp_service);
+                foreach ($service->event as $event) {
+                    $temp_event = array();
+                    $temp_event['language'] = array();
+                    $temp_event['content'] = array();
+                    $temp_event['parental_rating'] = array();
+                    $temp_event = self::formatAttribute($event->attributes(), $temp_event);
+                    foreach ($event->language as $language) {
+                        $temp_language = array();
+                        $temp_language['short_event'] = array();
+                        $temp_language = self::formatAttribute($language->attributes(), $temp_language);
+                        foreach ($language->short_event as $short_event) {
+                            $temp_short_event = array();
+                            $temp_short_event = self::formatAttribute($short_event->attributes(), $temp_short_event);
+                            array_push($temp_language['short_event'], $temp_short_event);
+                        }
+                        array_push($temp_event['language'], $temp_language);
+                    }
+
+                    foreach ($event->content as $content) {
+                        $temp_content = array();
+                        $temp_content = self::formatAttribute($content->attributes(), $temp_content);
+                        array_push($temp_event['content'], $temp_content);
+                    }
+
+                    foreach ($event->parental_rating as $parental_rating) {
+                        $temp_parental_rating = array();
+                        $temp_parental_rating['country'] = array();
+                        $temp_parental_rating = self::formatAttribute($parental_rating->attributes(), $temp_parental_rating);
+                        foreach ($language->country as $country) {
+                            $temp_country = array();
+                            $temp_country = self::formatAttribute($country->attributes(), $temp_country);
+                            array_push($temp_language['country'], $temp_country);
+                        }
+                        array_push($temp_event['parental_rating'], $temp_parental_rating);
+                    }
+                    array_push($temp_service['event'], $temp_event);
+                }
+                array_push($temp_network['service'], $temp_service);
+            }
+            array_push($array['network'], $temp_network);
+        }
+        return json_encode($array);
+    }
+}
